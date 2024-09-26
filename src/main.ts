6bb51326efab1f30a35dd54617a7e34f9b297c71
aramjs <aramjsaroyan@gmail.com>
2024-09-26 21:23:57 +0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 import linkRoutes from "./routes/linkRoutes";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+// Enable CORS for all routes
+app.use(cors()); // This will allow all origins. You can configure it further if needed.
 
 app.use("/api/links", linkRoutes);
 
